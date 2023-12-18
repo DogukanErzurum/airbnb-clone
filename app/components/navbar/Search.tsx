a4c9a4ef2b23af2e42ba617f47ed352c87848cae
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2023-12-18 17:15:42 +0300

    Added buttons to be added to Navbar in the first phase

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { BiSearch } from 'react-icons/bi';
+
 const Search = () => {
   return (
     <div 
@@ -31,6 +33,44 @@ const Search = () => {
           "
         >
           Anywhere
+        </div>
+        <div
+          className="
+            hidden
+            sm:block
+            text-sm
+            font-semibold
+            px-6
+            border-x-[1px]
+            flex-1
+            text-center
+          "
+        >
+          Any Week
+        </div>
+        <div
+          className="
+            text-sm
+            pl-6
+            pr-2
+            text-gray-600
+            flex
+            flex-row
+            items-center
+            gap-3
+          "
+        >
+          <div className="hidden sm:block">Add Guests</div>
+          <div
+            className="
+              p-2
+              bg-rose-500
+              rounded-full
+              text-white
+            "
+          >
+            <BiSearch size = {18} />
+          </div>
         </div>
       </div>
     </div>
