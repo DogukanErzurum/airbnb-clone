16cfa125b0c671718aec5d5820bab6c8b64071e1
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2023-12-18 16:44:09 +0300

    The infrastructure for the car button has been prepared

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
 
 
 const Navbar = () => {
@@ -23,6 +24,7 @@ const Navbar = () => {
                     "
                   >
                     <Logo />
+                    <Search />
                   </div>
                 </Container>
             </div>
