b59b5f609bcc1164c79f5de8bbc63bbb34a03490
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2023-12-19 23:04:16 +0300

    Design for the home screen has begin.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 import './globals.css'
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 export const metadata = {
   title: 'Airbnb',
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <Modal title="Hello World" isOpen/>
           <Navbar />
         </ClientOnly>
         {children}
