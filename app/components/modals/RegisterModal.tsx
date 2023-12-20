2b46f129b6729ef51733628b4fc71586693687d0
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2023-12-20 14:59:33 +0300

    Name, email and password sections have been added to become a member.

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -13,6 +13,7 @@ import {
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
+import Input from '../inputs/input';
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -52,6 +53,34 @@ const RegisterModal = () => {
       <Heading
         title="Welcome to Airbnb"
         subtitle="Create an account!"
+      />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+
       />
     </div>
   )
