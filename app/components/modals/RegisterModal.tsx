ceebe6e9f7135558ae8fdf2c148f3ed1d39dafe2
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2023-12-23 03:53:04 +0300

    The process of registering to the system with Github has been completed.

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -16,6 +16,7 @@ import Heading from '../Heading';
 import Input from '../inputs/input';
 import toast from 'react-hot-toast';
 import Button from '../Button';
+import { signIn } from 'next-auth/react';
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -100,7 +101,7 @@ const RegisterModal = () => {
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn('github')}
       />
       <div
         className="
