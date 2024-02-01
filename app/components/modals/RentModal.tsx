37c1885d4fdf3f1ca9fbda6a8085bb6f05dee365
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2024-02-01 13:33:02 +0300

    The necessary codes for selecting categories have started to be written.

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -5,6 +5,7 @@ import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
 import { useMemo, useState } from "react";
 import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
 
 enum STEPS {
     CATEGORY = 0,
@@ -47,10 +48,25 @@ const RentModal = () => {
   let bodyContent = (
     <div className="flex flex-col gap-8">
         <Heading 
-           title="Where is your place located?"
-           subtitle="Help guests find you!"
+           title="Which of these best describes your place?"
+           subtitle="Pick a category"
         />
-
+        <div
+          className="
+            grid
+            grid-cols-1
+            md:grid-cols-2
+            gap-3
+            max-h-[50vh]
+            overflow-y-auto
+          "
+        >
+          {categories.map((item) =>(
+            <div key = {item.label} className="col-span-1">
+              {item.label}
+            </div>
+          ))}
+        </div>
     </div>
   )
 
@@ -64,6 +80,7 @@ const RentModal = () => {
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
       title="Airbnb your home!"
+      body={bodyContent}
     />
   );
 }
