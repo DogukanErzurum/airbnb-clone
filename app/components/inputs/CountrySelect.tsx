8f3be0b98b869df2fab734817a63a5ad6c60c852
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2024-02-01 15:00:43 +0300

    Necessary procedures for location selection started to be carried out.

---
+++ app/components/inputs/CountrySelect.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import Select from 'react-select';
+
+export type CountrySelectValue = {
+  flag: string;
+  label: string;
+  latlng: number[];
+  region: string;
+  value: string;
+}
+
+const CountrySelect = () => {
+  return (
+    <div></div>
+  );
+}
+
+export default CountrySelect;
