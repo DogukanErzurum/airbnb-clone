852ab4d1e0faf7ee1fe18372c2be6000be06ad65
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2024-02-03 14:55:12 +0300

    price add complete

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,12 +7,13 @@ import { useMemo, useState } from "react";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
 import ImageUpload from "../inputs/ImageUpload";
 import Input from "../inputs/input";
+import axios from "axios";
 
 enum STEPS {
     CATEGORY = 0,
@@ -77,6 +78,16 @@ const RentModal = () => {
 
   const onNext = () => {
     setStep((value) => value + 1);
+  }
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    if (step !== STEPS.PRICE) {
+      return onNext();
+    }
+
+    setIsLoading(true);
+
+    axios.post('/api/listings', data)
   }
 
   const actionLabel = useMemo(() => {
@@ -207,8 +218,29 @@ const RentModal = () => {
         />
         <hr />
         <Input
-          id="title"
-          label="Title"
+          id="description"
+          label="Description"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.PRICE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Now, set your price"
+          subtitle="How much do you charge per night?"
+        />
+        <Input
+          id="price"
+          label="Price"
+          formatPrice 
+          type="number" 
           disabled={isLoading}
           register={register}
           errors={errors}
