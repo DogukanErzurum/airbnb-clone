048abd0dab60138d30077e5c8f4cc85b8847f187
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2024-02-04 13:26:02 +0300

    minor adjustments have been made to the listing of added houses

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+
 export default function Home() {
   return (
-    <div className="text-rose-500, text-2xl">Hello Airbnb!</div>
+    <ClientOnly>
+      <Container>
+        <div className="
+          pt-24
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2 
+          md:grid-cols-3 
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        ">
+          <div>My future listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
