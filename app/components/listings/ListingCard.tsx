120111cb97316f8aba957fa5da9450e0401b5e6b
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2024-02-06 01:04:02 +0300

    The data in the database was projected on the screen.

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -5,7 +5,9 @@ import { Listing, Reservation } from "@prisma/client";
 import { SafeUser } from "@/app/types";
 import { useRouter } from "next/navigation";
 import useCountries from "@/app/hooks/useCountries";
-import { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
+import { format } from 'date-fns';
+import Image from "next/image";
 
 interface ListingCardProps {
   data: Listing;
@@ -42,8 +44,60 @@ const ListingCard: React.FC<ListingCardProps> = ({
       onAction?.(actionId);
     }, [onAction, actionId, disabled])
 
+  const price = useMemo(() => {
+    if (reservation) {
+      return reservation.totalPrice;
+    }
+
+    return data.price;
+  }, [reservation, data.price]);
+
+  const reservationDate = useMemo(() => {
+    if (!reservation) {
+      return null;
+    }
+
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
+
+    return `${format(start, 'PP')} - ${format(end, 'PP')}`
+  }, [reservation]);
+
   return (
-    <div>Listing Card</div>
+    <div 
+      onClick={() => router.push(`/listings/${data.id}`)} 
+      className="
+        col-span-1 cursor-pointer group
+      "
+    >
+      <div className="flex flex-col gap-2 w-full">
+        <div
+          className="
+            aspect-square
+            w-full
+            relative
+            overflow-hidden
+            rounded-xl
+          "
+        >
+          <Image
+            fill
+            alt="Listing"
+            src={data.imageSrc}
+            className="
+              object-cover
+              h-full
+              w-full
+              group-hover:scale-110
+              transition  
+            "
+          />
+          <div className="">
+
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
