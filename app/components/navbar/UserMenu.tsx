07e0d353ffac9d90dbd516f14cfc4f6e28311bd9
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2024-02-08 20:43:03 +0300

    A page where the bookings made can be viewed was created.

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -9,6 +9,7 @@ import useLoginModal from '@/app/hooks/useLoginModal';
 import { signOut } from 'next-auth/react';
 import { SafeUser } from '@/app/types';
 import useRentModal from '@/app/hooks/useRentModal';
+import { useRouter } from 'next/navigation';
 
 interface UserMenuProps {
   currentUser?: SafeUser | null
@@ -17,6 +18,7 @@ interface UserMenuProps {
 const UserMenu: React.FC<UserMenuProps> = ({
   currentUser
 }) => {
+  const router = useRouter();
   const registerModal = useRegisterModal();  
   const loginModal = useLoginModal();
   const rentModal = useRentModal();
@@ -98,7 +100,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
             {currentUser ? (
               <>
               <MenuItem
-                onClick={() => {}}
+                onClick={() => router.push("/trips")}
                 label="My trips"
               />
               <MenuItem
