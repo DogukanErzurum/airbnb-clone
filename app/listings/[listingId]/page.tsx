f5cf1e7ee5e0fd1a332c04304d858dc849a5c962
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2024-02-08 20:07:58 +0300

    The process of the person who likes the house in the system clicking on the house and making a reservation from there has been completed. For other users, the dates selected in the calendar were made unselectable. When making a reservation, the total amount was placed under the calendar.

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,18 +1,21 @@
+
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
+import getReservations from "@/app/actions/getReservations";
 
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
 
 import ListingClient from "./ListingClient";
 
-
 interface IParams {
-    listingId?: string;
-  }
+  listingId?: string;
+}
 
 const ListingPage = async ({ params }: { params: IParams }) => {
+
   const listing = await getListingById(params);
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
 
   if (!listing) {
@@ -25,12 +28,13 @@ const ListingPage = async ({ params }: { params: IParams }) => {
 
   return (
     <ClientOnly>
-      <ListingClient 
+      <ListingClient
         listing={listing}
+        reservations={reservations}
         currentUser={currentUser}
       />
     </ClientOnly>
   );
 }
-
+ 
 export default ListingPage;
