a9a4c4e5b5657f12a806faabda81137b46ddb1e5
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2024-02-08 20:42:58 +0300

    A page where the bookings made can be viewed was created.

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { Listing, Reservation } from "@prisma/client";
-
-import { SafeListing, SafeUser } from "@/app/types";
+import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
 import { useRouter } from "next/navigation";
 import useCountries from "@/app/hooks/useCountries";
 import React, { useCallback, useMemo } from "react";
@@ -13,7 +11,7 @@ import Button from "../Button";
 
 interface ListingCardProps {
   data: SafeListing;
-  reservation?: Reservation;
+  reservation?: SafeReservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
   actionLabel?: string;
