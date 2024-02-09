d52e059aaf31db6348c8999d7bee498222b28205
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2024-02-09 14:45:42 +0300

    the page to list the favourited houses has been completed

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -104,7 +104,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                 label="My trips"
               />
               <MenuItem
-                onClick={() => {}}
+                onClick={() => router.push('/favorites')}
                 label="My favorites"
               />
               <MenuItem
