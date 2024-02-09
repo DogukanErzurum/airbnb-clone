01347f7bfda6c3f121a3b20eda39d024d0396c84
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2024-02-09 20:20:39 +0300

    The filtering part (assigning various filters, adding advanced query logic to getListings, adding loading and error pages) has been completed. As a result of these operations, the project was successfully completed.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,24 @@
-import { Nunito } from "next/font/google";
+import { Nunito } from 'next/font/google'
+
+import Navbar from '@/app/components/navbar/Navbar';
+import LoginModal from '@/app/components/modals/LoginModal';
+import RegisterModal from '@/app/components/modals/RegisterModal';
+import SearchModal from '@/app/components/modals/SearchModal';
+import RentModal from '@/app/components/modals/RentModal';
+
+import ToasterProvider from '@/app/providers/ToasterProvider';
 
 import './globals.css'
-import Navbar from "./components/navbar/Navbar";
-import ClientOnly from "./components/ClientOnly";
-import RegisterModal from "./components/modals/RegisterModal";
-import ToasterProvider from "./providers/ToasterProvider";
-import LoginModal from "./components/modals/LoginModal";
-import getCurrentUser from "./actions/getCurrentUser";
-import RentModal from "./components/modals/RentModal";
+import ClientOnly from './components/ClientOnly';
+import getCurrentUser from './actions/getCurrentUser';
 
 export const metadata = {
   title: 'Airbnb',
   description: 'Airbnb Clone',
 }
 
-const font = Nunito({
-  subsets: ["latin"],
+const font = Nunito({ 
+  subsets: ['latin'], 
 });
 
 export default async function RootLayout({
@@ -30,15 +33,16 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
-          <RentModal />
           <LoginModal />
           <RegisterModal />
-          <Navbar currentUser={currentUser}/>
+          <SearchModal />
+          <RentModal />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         <div className="pb-20 pt-28">
           {children}
         </div>
-        </body>
+      </body>
     </html>
   )
 }
